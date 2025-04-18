7e0ac5b715afcbba9fd34cdd588f0b27a605504d
Nimesh <nimesh.ghimire99@hotmail.com>
2025-04-18 13:27:10 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author,on_delete = models.CASCADE ,blank=True, null=True)
+    categories = models.ManyToManyField(Category, blank=True)
+
+    def __str__(self):
+        return self.name
