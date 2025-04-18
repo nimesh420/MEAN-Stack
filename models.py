d9accc9d3f21c956c8d25217e4e0702d953e37d7
Nimesh Ghimire <smnighim@gmail.com>
2025-04-18 13:44:47 +0200

    second commit

--- models.py
+++ models.py
@@ -7,20 +7,21 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
+
 class Book(models.Model):
     name = models.CharField(max_length=100)
-    author = models.ForeignKey(Author,on_delete = models.CASCADE ,blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, blank=True, null=True)
     categories = models.ManyToManyField(Category, blank=True)
 
     def __str__(self):
         return self.name
-    
 
 
-hello world this is error i added in my models.
+# hello world this is error i added in my models. added comment
